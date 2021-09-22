2dc71a9b590a8a1a8e8b59648591b4dfb4f1627f
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-09-22 20:17:28 +0900

    fix: favicon

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -29,13 +29,13 @@ const resource = [
   /* --- Favicons --- */
   {% assign favicon_path = "/assets/img/favicons" | relative_url %}
 
-  '{{ favicon_path }}/android-chrome-192x192.png',
-  '{{ favicon_path }}/android-chrome-512x512.png',
-  '{{ favicon_path }}/apple-touch-icon.png',
+  '{{ favicon_path }}/android-icon-192x192.png',
+  '{{ favicon_path }}/android-icon-512x512.png',
+  '{{ favicon_path }}/apple-icon-180x180.png',
   '{{ favicon_path }}/favicon-16x16.png',
   '{{ favicon_path }}/favicon-32x32.png',
   '{{ favicon_path }}/favicon.ico',
-  '{{ favicon_path }}/mstile-150x150.png',
+  '{{ favicon_path }}/ms-icon-150x150.png',
   '{{ favicon_path }}/site.webmanifest',
   '{{ favicon_path }}/browserconfig.xml'
 
